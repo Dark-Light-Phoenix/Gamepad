4b4086f5b6fe0b1e42609221452b4c735e7dd20f
Dark-Light-Phoenix <155775130+Dark-Light-Phoenix@users.noreply.github.com>
2024-08-21 00:15:32 +0300

    Added function that would be read status of pin for turning on/off LED

--- Core/Inc/Backlight,Menu,NRST.h
+++ Core/Inc/Backlight,Menu,NRST.h
@@ -1,6 +1,6 @@
 #ifndef INC_BACKLIGHT_MENU_NRST_H_
 #define INC_BACKLIGHT_MENU_NRST_H_
 
-
+void Backlight (float *Backlight);
 
 #endif /* INC_BACKLIGHT_MENU_NRST_H_ */
--- Core/Src/Backlight,Menu,NRST.c
+++ Core/Src/Backlight,Menu,NRST.c
@@ -1,2 +1,8 @@
 #include "Backlight,Menu,NRST.h"
 #include "stm32g4xx_it.h"
+
+void Backlight (float *Backlight)
+{
+	HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_5);
+	HAL_Delay (500);
+}
