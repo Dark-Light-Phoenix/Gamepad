57d0267f624bfa6935f514794b0fb40b1c701ff9
Dark-Light-Phoenix <155775130+Dark-Light-Phoenix@users.noreply.github.com>
2024-08-21 00:31:46 +0300

    Added function for Left Trigger

--- Core/Inc/Triggers.h
+++ Core/Inc/Triggers.h
@@ -1,6 +1,6 @@
 #ifndef INC_TRIGGERS_H_
 #define INC_TRIGGERS_H_
 
-
+void Left_Trigger (float *L1);
 
 #endif /* INC_TRIGGERS_H_ */
--- Core/Src/Triggers.c
+++ Core/Src/Triggers.c
@@ -1,5 +1,8 @@
 #include "Triggers.h"
 #include "stm32g4xx_it.h"
 
+void Left_Trigger (float *L1)
+{
 
+}
 
