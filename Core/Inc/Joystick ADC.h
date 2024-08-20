e890d0dbee8541930686855509f6c622ef2923e7
Dark-Light-Phoenix <155775130+Dark-Light-Phoenix@users.noreply.github.com>
2024-08-21 00:21:45 +0300

    Added extern of adc variables

--- Core/Inc/Joystick ADC.h
+++ Core/Inc/Joystick ADC.h
@@ -1,6 +1,6 @@
 #ifndef INC_JOYSTICK_ADC_H_
 #define INC_JOYSTICK_ADC_H_
 
-
+void ADC_Left (void);
 
 #endif /* INC_JOYSTICK_ADC_H_ */
--- Core/Src/Joystick ADC.c
+++ Core/Src/Joystick ADC.c
@@ -1,2 +1,10 @@
 #include "Joystick ADC.h"
 #include "stm32g4xx_it.h"
+
+extern ADC_HandleTypeDef hadc1;
+extern ADC_HandleTypeDef hadc2;
+
+void ADC_Left (void)
+{
+
+}
