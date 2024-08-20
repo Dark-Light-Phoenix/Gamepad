d246daca6b958b7fb7b71739a083d206b398c423
Dark-Light-Phoenix <155775130+Dark-Light-Phoenix@users.noreply.github.com>
2024-08-21 00:35:04 +0300

    Added function for Left Motor

--- Core/Inc/Vibromotors.h
+++ Core/Inc/Vibromotors.h
@@ -1,6 +1,6 @@
 #ifndef INC_VIBROMOTORS_H_
 #define INC_VIBROMOTORS_H_
 
-
+void Left_Motor (void);
 
 #endif /* INC_VIBROMOTORS_H_ */
--- Core/Src/Vibromotors.c
+++ Core/Src/Vibromotors.c
@@ -1,2 +1,7 @@
 #include "Vibromotors.h"
 #include "stm32g4xx_it.h"
+
+void Left_Motor (void)
+{
+
+}
