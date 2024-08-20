ecf207d6445aad2ba6ac1b4c2452d6b04aded90a
Dark-Light-Phoenix <155775130+Dark-Light-Phoenix@users.noreply.github.com>
2024-08-21 00:28:59 +0300

    Added function for setting color of LED

--- Core/Inc/RGB.h
+++ Core/Inc/RGB.h
@@ -1,6 +1,6 @@
 #ifndef INC_RGB_H_
 #define INC_RGB_H_
 
-
+void Set_Color (void);
 
 #endif /* INC_RGB_H_ */
--- Core/Src/RGB.c
+++ Core/Src/RGB.c
@@ -1,3 +1,7 @@
 #include "RGB.h"
 #include "stm32g4xx_it.h"
 
+void Set_Color (void)
+{
+
+}
