a8406bf1a4f772eb81aee1e3fc9d619bf5296d69
Dark-Light-Phoenix <155775130+Dark-Light-Phoenix@users.noreply.github.com>
2024-08-21 00:05:52 +0300

    Added base functions for Connecting and disconnecting gamepad throught Typre-C

--- Core/Inc/Type-C.h
+++ Core/Inc/Type-C.h
@@ -1,6 +1,8 @@
 #ifndef INC_TYPE_C_H_
 #define INC_TYPE_C_H_
 
-
+void USB_Connect (PCD_HandleTypeDef *hpcd_USB_FS);
+void USB_Disconnect (PCD_HandleTypeDef *hpcd_USB_FS);
+void USB_Set_Address (PCD_HandleTypeDef *hpcd_USB_FS, uint8_t address);
 
 #endif /* INC_TYPE_C_H_ */
--- Core/Src/Type-C.c
+++ Core/Src/Type-C.c
@@ -1,2 +1,17 @@
 #include "Type-C.h"
 #include "stm32g4xx_it.h"
+
+void USB_Connect (PCD_HandleTypeDef *hpcd_USB_FS)
+{
+	HAL_PCD_DevConnect(hpcd_USB_FS);
+}
+
+void USB_Disconnect (PCD_HandleTypeDef *hpcd_USB_FS)
+{
+	HAL_PCD_DevDisconnect(hpcd_USB_FS);
+}
+
+void USB_Set_Address (PCD_HandleTypeDef *hpcd_USB_FS, uint8_t address)
+{
+	HAL_PCD_SetAddress(hpcd_USB_FS, address);
+}
